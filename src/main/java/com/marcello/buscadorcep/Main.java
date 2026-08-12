51e19a9e9828fa5a1263cd7ba567675a1104572c
mbarros02 <barroscello123@gmail.com>
2026-08-12 09:54:32 -0300

    first commit

---
+++ src/main/java/com/marcello/buscadorcep/Main.java
@@ -0,0 +1,4 @@
+package main.java.com.marcello.buscadorcep;
+
+public class Main {
+}
---
+++ src/main/java/com/marcello/buscadorcep/model/Endereco.java
@@ -0,0 +1,40 @@
+package main.java.com.marcello.buscadorcep.model;
+
+public class Endereco {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+}
