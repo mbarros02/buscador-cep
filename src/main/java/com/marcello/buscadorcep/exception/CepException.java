9587cf65ecfc00892f5a241d92ad788b100fbb37
Marcello Barros <barroscello123@gmail.com>
2026-09-12 18:10:21 -0300

    Criação das classes de exceções.

---
+++ src/main/java/com/marcello/buscadorcep/exception/CepException.java
@@ -0,0 +1,7 @@
+package main.java.com.marcello.buscadorcep.exception;
+
+public class CepException extends RuntimeException {
+    public CepException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/marcello/buscadorcep/exception/CepInvalidoException.java
@@ -0,0 +1,7 @@
+package main.java.com.marcello.buscadorcep.exception;
+
+public class CepInvalidoException extends RuntimeException {
+    public CepInvalidoException(String message) {
+        super(message);
+    }
+}
