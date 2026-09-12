9b183225c19496f9c351fe9dbc1b770287e2734e
Marcello Barros <barroscello123@gmail.com>
2026-09-12 19:25:35 -0300

    Criação do menu de interação com o usuário.

--- src/main/java/com/marcello/buscadorcep/exception/CepInvalidException.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.marcello.buscadorcep.exception;
-
-public class CepInvalidException {
-}
---
+++ src/main/java/com/marcello/buscadorcep/menuCli/Menu.java
@@ -0,0 +1,4 @@
+package main.java.com.marcello.buscadorcep.menuCli;
+
+public class Menu {
+}
